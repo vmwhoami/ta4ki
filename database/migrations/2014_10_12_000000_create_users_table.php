3bc53184b5510fc890bb9a11030de75f7a39a2e8
vmwhoami <vmwhoami@gmail.com>
2021-07-14 22:29:38 +0300

    Play with custom object creation

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -16,6 +16,8 @@ class CreateUsersTable extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('name');
+            $table->smallInteger('phone_number');
+            $table->string('city');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
